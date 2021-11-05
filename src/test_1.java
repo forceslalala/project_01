5aeb2b0484547ca1efd9543ce87add71e1c56c11
王涛 <1045257201@qq.com>
2021-11-05 15:21:00 +0800

    测试提交

--- src/test_1.java
+++ src/test_1.java
@@ -1,7 +1,6 @@
 public class test_1 {
     public static void main(String[] args){
-        System.out.println("Programming is fun!");
-        System.out.println("Fundamentals First");
-        System.out.println("Problem Driven");
+        System.out.print("(10.5 + 2 * 3)/(45 - 3.5) = ");
+        System.out.println((10.5 + 2 * 3)/(45 - 3.5));
     }
 }
