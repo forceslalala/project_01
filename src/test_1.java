af187fbb474baf3a881ba710064339046d3457fc
王涛 <1045257201@qq.com>
2021-11-05 16:28:40 +0800

    测试提交

--- src/test_1.java
+++ src/test_1.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 public class test_1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter an integer for seconds: ");
-        int seconds = input.nextInt();
-        int minutes = seconds / 60;
-        int remainingSeconds = seconds % 60;
-        System.out.println(seconds + " seconds is " + minutes +
-                " minutes and " + remainingSeconds + " seconds");
+        System.out.print("Enter a degree in Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        System.out.println("Fahrenheit " + fahrenheit + " is " +
+                celsius + " in Celsius");
     }
 }
