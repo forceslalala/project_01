0613e4a01a505496359b6c6d795340ec23ede33f
王涛 <1045257201@qq.com>
2021-11-05 15:15:21 +0800

    测试提交

--- src/test_1.java
+++ src/test_1.java
@@ -1,5 +1,7 @@
 public class test_1 {
     public static void main(String[] args){
-        System.out.println("Welcome to Java!");
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
     }
 }
