c29b6393a03f4e0339aaa2f8a00ed86169d18943
王涛 <1045257201@qq.com>
2021-11-05 15:34:07 +0800

    测试提交

--- src/test_1.java
+++ src/test_1.java
@@ -1,6 +1,6 @@
 public class test_1 {
     public static void main(String[] args){
-        System.out.print("(10.5 + 2 * 3)/(45 - 3.5) = ");
-        System.out.println((10.5 + 2 * 3)/(45 - 3.5));
+        System.out.print("Celsius 35 is Fahrenheit degree");
+        System.out.println((9.0 / 5) * 35 + 32);
     }
 }
