8b0517f7219c912a6cbf912127ba60ce9b8cd88c
王涛 <1045257201@qq.com>
2021-11-05 16:13:39 +0800

    测试提交

--- src/test_1.java
+++ src/test_1.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class test_1 {
     public static void main(String[] args){
-        final double PI = 3.14159;
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number for radius: ");
-        double radius = input.nextDouble();
-        double area = radius *radius * PI;
-        System.out.println("The area for the circle of radius " +
-                radius + " is " + area);
+        System.out.print("Enter an integer for seconds: ");
+        int seconds = input.nextInt();
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+        System.out.println(seconds + " seconds is " + minutes +
+                " minutes and " + remainingSeconds + " seconds");
     }
 }
