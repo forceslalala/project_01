b470e5345e36e4788dfb3a6a92c8f3da3171e873
王涛 <1045257201@qq.com>
2021-11-08 18:56:21 +0800

    测试提交

--- src/test_1.java
+++ src/test_1.java
@@ -1,13 +1,10 @@
+import java.util.Scanner;
 public class test_1 {
     public static void main(String[] args) {
-        long totalMilliseconds = System.currentTimeMillis();
-        long totalSeconds = totalMilliseconds / 1000;
-        long currentSecond = totalSeconds % 60;
-        long totalMinutes = totalSeconds / 60;
-        long currentMinute = totalMinutes % 60;
-        long totalHours = totalMinutes / 60;
-        long currentHour = totalHours % 24;
-        System.out.println("Current time is " + currentHour + ":"
-            + currentMinute + ":" + currentSecond + " GMT");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter purchase amount: ");
+        double purchaseAmount = input.nextDouble();
+        double tax = purchaseAmount * 0.06;
+        System.out.println("Salas tax is $" + (int)(tax * 100) / 100.0);
     }
 }
