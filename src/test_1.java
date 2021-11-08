5091dd438c5b0b65c0bb0bb00143077b6aca82aa
王涛 <1045257201@qq.com>
2021-11-08 20:20:16 +0800

    测试提交

--- src/test_1.java
+++ src/test_1.java
@@ -2,20 +2,24 @@ import java.util.Scanner;
 public class test_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter annual interest rate, e.g., 7.25: ");
-        double annualInterestRate = input.nextDouble();
-        double monthlyInterestRate = annualInterestRate / 1200;
         System.out.print(
-                "Enter number of years as an integer, e.g., 5: ");
-        int numberOfYears = input.nextInt();
-        System.out.print("Enter loan amount, e.g., .95: ");
-        double loanAmount = input.nextDouble();
-        double monthlyPayment = loanAmount * monthlyInterestRate / (1
-                -1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12));
-        double totalPayment = monthlyPayment * numberOfYears * 12;
-        System.out.println("The monthly payment is $" +
-                (int)(monthlyPayment * 100) / 100.0);
-        System.out.println("The total payment is $" +
-                (int)(totalPayment * 100) / 100.0);
+                "Enter an amount in double, for example 11.5: ");
+        double amount = input.nextDouble();
+        int remainingAmount = (int)(amount * 100);
+        int numberOfOneDollars = remainingAmount / 100;
+        remainingAmount = remainingAmount % 100;
+        int numberOfQuarters = remainingAmount / 25;
+        remainingAmount = remainingAmount % 25;
+        int numberOfDimes = remainingAmount / 10;
+        remainingAmount = remainingAmount % 10;
+        int numberOfNickels = remainingAmount / 5;
+        remainingAmount = remainingAmount % 5;
+        int number0fPennies = remainingAmount;
+        System.out.println("Your amount " + amount + " consists of" );
+        System.out.println(" " + numberOfOneDollars + " dollars");
+        System.out.println(" " + numberOfQuarters + " quarters");
+        System.out.println(" " + numberOfDimes + " dimes");
+        System.out.println(" " + numberOfNickels + " nickels");
+        System.out.println(" " + number0fPennies + " pennies");
     }
 }
