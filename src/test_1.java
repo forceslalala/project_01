0175fabfb822d044fe731c0d87366f7a3d9ccc3d
王涛 <1045257201@qq.com>
2021-11-08 19:18:02 +0800

    测试提交

--- src/test_1.java
+++ src/test_1.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 public class test_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter purchase amount: ");
-        double purchaseAmount = input.nextDouble();
-        double tax = purchaseAmount * 0.06;
-        System.out.println("Salas tax is $" + (int)(tax * 100) / 100.0);
+        System.out.print("Enter annual interest rate, e.g., 7.25: ");
+        double annualInterestRate = input.nextDouble();
+        double monthlyInterestRate = annualInterestRate / 1200;
+        System.out.print(
+                "Enter number of years as an integer, e.g., 5: ");
+        int numberOfYears = input.nextInt();
+        System.out.print("Enter loan amount, e.g., .95: ");
+        double loanAmount = input.nextDouble();
+        double monthlyPayment = loanAmount * monthlyInterestRate / (1
+                -1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12));
+        double totalPayment = monthlyPayment * numberOfYears * 12;
+        System.out.println("The monthly payment is $" +
+                (int)(monthlyPayment * 100) / 100.0);
+        System.out.println("The total payment is $" +
+                (int)(totalPayment * 100) / 100.0);
     }
 }
