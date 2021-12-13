791da21cd147894a4e975f43d85eb41d6337274e
王涛 <1045257201@qq.com>
2021-12-13 20:27:48 +0800

    测试提交

--- src/zuoye_5/test_9_11.java
+++ src/zuoye_5/test_9_11.java
@@ -1,8 +1,27 @@
 package zuoye_5;
 
+import java.util.Scanner;
+
 public class test_9_11 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int d = input.nextInt();
+        int e = input.nextInt();
+        int f = input.nextInt();
 
+        LinearEquation lin = new LinearEquation(a,b,c,d,e,f);
+
+        boolean bool = lin.isSolvable();
+
+        if(bool){
+            System.out.println("x is " + lin.getX() + " and y is " + lin.getY());
+        }
+        else{
+            System.out.println("The equation has no solution");
+        }
     }
 }
 
@@ -19,6 +38,62 @@ class LinearEquation{
     }
 
     double getA(){
+        return a;
+    }
 
+    double getB(){
+        return b;
+    }
+
+    double getC(){
+        return c;
+    }
+
+    double getD(){
+        return d;
+    }
+
+    double getE(){
+        return e;
+    }
+
+    double getF(){
+        return f;
+    }
+
+    void setA(double a){
+        this.a = a;
+    }
+
+    void setB(double b){
+        this.b = b;
+    }
+
+    void setC(double c){
+        this.c = c;
+    }
+
+    void setD(double d){
+        this.d = d;
+    }
+
+    void setE(double e){
+        this.e = e;
+    }
+
+    void setF(double f){
+        this.f = f;
+    }
+
+    boolean isSolvable(){
+        return (a * d - b * c) != 0;
+    }
+
+    double getX(){
+        return (e * d - b * f) / (a * d - b * c);
+    }
+
+    double getY(){
+        return (a * f - e * c) / (a * d - b * c);
     }
 }
