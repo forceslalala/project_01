cd5b5c488e5c796b832e442f7ecf378e942cf907
王涛 <1045257201@qq.com>
2021-12-13 20:12:06 +0800

    测试提交

--- src/zuoye_5/test_9_11.java
+++ src/zuoye_5/test_9_11.java
@@ -1,5 +1,24 @@
 package zuoye_5;
 
 public class test_9_11 {
+    public static void main(String[] args) {
 
+    }
+}
+
+class LinearEquation{
+    private double a,b,c,d,e,f;
+
+    LinearEquation(double a,double b,double c,double d,double e,double f){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+    }
+
+    double getA(){
+
+    }
 }
